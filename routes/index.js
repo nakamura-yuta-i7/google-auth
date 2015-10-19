dc8a211edac207619969259499755e49389325af
Yuta Nakamura <yuta.nakamura.i7@gmail.com>
2015-10-19 03:55:57 +0000

    materialization

--- routes/index.js
+++ routes/index.js
@@ -7,9 +7,24 @@ router.get('/', function(req, res, next) {
 	console.log( __filename, "req.user:", req.user );
 	console.log( __filename, "req.session:", req.session );
 	var user = []
-	var data = { title: 'Express' };
+	var data = {
+		siteName: "Oauth Login Demo",
+		title: 'Home',
+		loggedin: ( req.user ? true : false ),
+	};
 	
 	res.render('index', data);
+});
+
+router.get('/login', function(req, res, next) {
+	console.log( __filename, "req.user:", req.user );
+	console.log( __filename, "req.session:", req.session );
+	var user = []
+	var data = {
+		title: 'Oauth Login Demo'
+	};
+	
+	res.render('index_login', data);
 });
 
 module.exports = router;
